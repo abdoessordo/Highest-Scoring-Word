156d5d4cb2843738490a745df20a359aefd26aac
abdoessordo <62575309+abdoessordo@users.noreply.github.com>
2020-10-16 20:39:23 +0100

    Create highest_scoring_word.py

---
+++ highest_scoring_word.py
@@ -0,0 +1,42 @@
+def high(x):
+    letters = {
+        'a':1,
+        'b':2,
+        'c':3,
+        'd':4,
+        'e':5,
+        'f':6,
+        'g':7,
+        'h':8,
+        'i':9,
+        'j':10,
+        'k':11,
+        'l':12,
+        'm':13,
+        'n':14,
+        'o':15,
+        'p':16,
+        'q':17,
+        'r':18,
+        's':19,
+        't':20,
+        'u':21,
+        'v':22,
+        'w':23,
+        'x':24,
+        'y':25,
+        'z':26
+        }
+    score = 0
+    temp = []
+    x = x.split()
+
+    for world in x:
+        for i in world:
+            score += letters[i]
+        temp.append(score)
+        score = 0
+
+    return x[temp.index(max(temp))]
+
+
